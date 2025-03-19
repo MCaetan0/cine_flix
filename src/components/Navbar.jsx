1de607fb2403c830ed8c0b3c299ea5dfe05a5e53
MCaetan0 <marceloc.lima349@gmail.com>
2025-03-19 16:53:46 -0300

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">MoviesLib</Link>
+      </h2>
+      <form></form>
+    </nav>
+  );
+};
+
+export default Navbar;
